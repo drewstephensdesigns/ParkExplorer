dbcaf76ed342fc50055318fa170398b59a83a2d4
Drew <usafcrewchief06@yahoo.com>
2023-09-02 17:05:19 -0500

    Update Config.kt
    Removed API Key from repo

--- app/src/main/java/com/github/drewstephensdesigns/parkexplorer/utils/Config.kt
+++ app/src/main/java/com/github/drewstephensdesigns/parkexplorer/utils/Config.kt
@@ -2,7 +2,10 @@ package com.github.drewstephensdesigns.parkexplorer.utils
 
 object Config {
 
+    // Get API at https://www.nps.gov/subjects/developer/get-started.htm
+    const val API_KEY = ""
+    
     // URL for endpoint for activities within parks
-    const val BASE_URL = "https://developer.nps.gov/api/v1/topics/parks?&q=archeology%2C%20animals%2C%20tragic%20events&limit=1000&api_key=nS9j5dUMcTGfi1EPgW3E62uzz4bmwdF2vquuj5DL"
+    const val BASE_URL = "https://developer.nps.gov/api/v1/topics/parks?&q=archeology%2C%20animals%2C%20tragic%20events&limit=1000&api_key=$API_KEY"
 
 }
